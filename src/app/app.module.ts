4cdcdb3744acdabda0ef705aa1ca535067bdc77e
Sarah Maris <sarah.maris@udacity.com>
2021-02-06 15:00:00 -0800

    L2, Navigation, The Angular Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component';
 import { PostItemComponent } from './post-item/post-item.component';
+import { HistoryComponent } from './history/history.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostsComponent,
-    PostItemComponent
+    PostItemComponent,
+    HistoryComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,

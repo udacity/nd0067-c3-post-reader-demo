bc66c61bb43b946909750af4b0d72d4bf231039b
Sarah Maris <sarah.maris@udacity.com>
2021-02-06 12:00:00 -0800

    L2, Sending Data to a Child Component, Passing Data From Parent to Child: @Input

---
+++ src/app/post-item/post-item.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Post } from '../models/Post';
+
+@Component({
+  selector: 'app-post-item',
+  templateUrl: './post-item.component.html',
+  styleUrls: ['./post-item.component.css']
+})
+export class PostItemComponent implements OnInit {
+  @Input() post: Post;
+
+  constructor() {
+    this.post = {
+      id: 0,
+      title: '',
+      body: '',
+      votes: 1
+    }
+  }
+
+  ngOnInit(): void {
+  }
+
+}

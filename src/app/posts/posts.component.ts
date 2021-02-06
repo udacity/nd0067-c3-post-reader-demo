dab059cf27c038125b4ddafc00085c15f9032461
Sarah Maris <sarah.maris@udacity.com>
2021-02-06 10:00:00 -0800

    L2, The Component Lifecycle, ngOnInit()

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from '../models/Post';
 
 @Component({
   selector: 'app-posts',
@@ -7,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostsComponent implements OnInit {
   title: string = 'Posts';
+  posts: Post[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.posts = [
+      {
+        id: 1,
+        title: "My first post",
+        body: "Just testing this out!",
+        votes: 1
+      },
+      {
+        id: 2,
+        title: "What's your favorite front-end framework?",
+        body: "My favorite is Angular. What do you enjoy working with?",
+        votes: 1
+      },
+      {
+        id: 3,
+        title: "ngOnInit is great",
+        body: "This lifecycle method gets called automatically upon component initialization!",
+        votes: 1
+      },
+    ];
   }
 
 }
